4727fc39705ad9652c126922fa8e66659002cde3
gaurav <=>
2026-09-08 11:58:02 +0530

    New changes

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -14,7 +14,9 @@ public class BaseClass {
     public WebDriver setUp() {
         ConfigReader reader= new ConfigReader();
         String urlOpen =  reader.getPropertyValue("url");
-        System.out.println("Url is.." + urlOpen);
+        System.out.println("Url is..1" + urlOpen);
+        System.out.println("Url is..2" + urlOpen);
+        System.out.println("Url is..3" + urlOpen);
         String maven_bro = System.getProperty("browser");
         if (driver == null) {
 
